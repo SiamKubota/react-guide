3c51191c4e24d9ab1ca9ab57aae2c26e734f1be5
Chachchai Tongthavornsuwan <chachchai.to@siamkubota.co.th>
2024-01-12 10:41:09 +0700

    :sparkles: Adding Components and Props

--- 02_components-and-props/final/src/App.jsx
+++ 02_components-and-props/final/src/App.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 
-function ProgrammingCard() {
+function ProgrammingCard(props) {
+  const [liked, setLiked] = useState(false);
+
+  console.log("props: ", props);
+
+  function onClickLike(event) {
+    props.onChangeLike(props.programmingName);
+    setLiked(event.target.checked);
+  }
+
   return (
     <section className="container">
-      <h1>Javascript</h1>
+      <p>{props.programmingName}</p>
+      <input type="checkbox" value={liked} onChange={onClickLike} />
       <ul>
-        <li>React</li>
-        <li>Vue</li>
-        <li>Angular</li>
+        <li>{props.lib1}</li>
+        <li>{props.lib2}</li>
+        <li>{props.lib3}</li>
       </ul>
     </section>
   );
 }
 
 function App() {
+  function alertClickChildrenComp(programmingName) {
+    alert("You click like " + programmingName);
+  }
+
   return (
-    <>
-      <div>
-        <h1>Programming Languages and Toools</h1>
-        <ProgrammingCard />
-        <ProgrammingCard />
-        <ProgrammingCard />
-      </div>
-    </>
+    <div>
+      <h1>Programming languages and tools</h1>
+      <ProgrammingCard
+        programmingName="Javacript"
+        lib1="React"
+        lib2="Angular"
+        lib3="Vue"
+        onChangeLike={alertClickChildrenComp}
+      />
+      <ProgrammingCard
+        programmingName="Python"
+        lib1="Django"
+        lib2="FastAPI"
+        onChangeLike={alertClickChildrenComp}
+      />
+      <ProgrammingCard
+        programmingName="Java"
+        lib1="Spring"
+        onChangeLike={alertClickChildrenComp}
+      />
+    </div>
   );
 }
 
