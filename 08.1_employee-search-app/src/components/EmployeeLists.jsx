13eded0f9018d4197a1ba0469536045e44e8b0d7
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2024-01-31 09:31:59 +0700

    :construction: Add Card comp to Lists

--- 08.1_employee-search-app/src/components/EmployeeLists.jsx
+++ 08.1_employee-search-app/src/components/EmployeeLists.jsx
@@ -1,11 +1,45 @@
+import Grid from "@mui/material/Grid";
+import MuiCard from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+import { styled } from "@mui/material/styles";
+
+const Card = styled(MuiCard)({
+  display: "flex",
+  alignItems: "center",
+  height: "100%",
+});
+
 export default function EmployeeLists(props) {
   const { employees = [] } = props;
 
   return (
-    <div>
+    <Grid container spacing={2}>
       {employees.map((employee) => (
-        <p>{employee.eid}</p>
+        <Grid key={employee.eid} item xs={12} sm={6}>
+          <Card>
+            <CardMedia
+              component="img"
+              alt="employee_avatar"
+              image={employee.picture_url}
+              sx={{ objectFit: "contain", width: { xs: 80, sm: 120 } }}
+            />
+            <CardContent>
+              <Typography variant="subtitle1">
+                {`${employee.titleTH}${employee.nameTH} ${
+                  employee.lastnameTH
+                } ${
+                  employee.nicknameTH ? `(${employee.nicknameTH})` : ""
+                }`}{" "}
+              </Typography>
+              <Typography variant="subtitle2">{employee.eid}</Typography>
+              <Typography variant="subtitle2">{employee.jobNameTH}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
